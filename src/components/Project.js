86a604ad4046419f1f5ad62440866a407f3e0fab
zhengjianliu <comezheng@gmail.com>
2020-12-21 23:10:56 -0500

    bug fixed

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react'
 
-class Projects extends Component{
-  render(){
-    return(
-      <div className="project">
-        <h1>heyhey</h1>
+class Projects extends Component {
+  render() {
+    return (<div className="project">
+      <div className="container">
+        <img src={this.props.image} alt={this.props.alt} className="image"/>
+        <div className="overlay">
+          <div className="text"><b>Project Name:</b> {this.props.desp}</div>
+        </div>
       </div>
-    )
+    </div>)
   }
 }
 
--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,22 +1,25 @@
 import React, {Component} from 'react'
 import Project from '../components/Project'
+// import image from '../images/js.png'
 
-class Projects extends Component{
-  render(){
-    return(
-      <div id="projects" className="fullpage">
-        <h1 className={this.props.open?"pagemark":"pagemark remove"}>Projects</h1>
-        <Project/>
-        <Project/>
-        <Project/>
-        <Project/>
+class Projects extends Component {
 
-      <Project/>
-      <Project/>
-      
-
+  render() {
+    let image = "https://llandscapes-10674.kxcdn.com/wp-content/uploads/2019/07/lighting.jpg"
+    return (<div id="projects" className="fullpage">
+      <h1 className={this.props.open
+          ? "pagemark"
+          : "pagemark remove"}>Projects</h1>
+      <div className="projectcontainer">
+        <Project image={image} alt={"project image"} desp={"Zip Trade"}/>
+        <Project image={image} alt={"project image"} desp={"BestBey"}/>
+        <Project image={image} alt={"project image"} desp={"YouPlay"}/>
+        <Project image={image} alt={"project image"} desp={"Portfolio Website"}/>
+        <Project image={image} alt={"project image"} desp={"Portfolio Website"}/>
+        <Project image={image} alt={"project image"} desp={"Portfolio Website"}/>
       </div>
-    )
+
+    </div>)
   }
 }
 
