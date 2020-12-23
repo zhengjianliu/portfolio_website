0d4162b03ede60708be0e600a6edb7600854eda3
zhengjianliu <comezheng@gmail.com>
2020-12-22 22:30:49 -0500

    loading page added

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Projects from './containers/Projects'
 import Contact from './containers/Contact'
 import Navbar from './containers/Navbar'
 import Footer from './containers/Footer'
+import Loading from './containers/Loading'
 
 class App extends Component{
   state={
     open: true,
+    loading:false,
   }
 
   clickHandler = () =>{
@@ -19,16 +21,27 @@ class App extends Component{
   closeHandler = () =>{
     this.setState({open:true})
   }
+  finishloading= (percentage) =>{
+    if(percentage>100){
+      this.setState({loading:true})
+    }
+  }
 
   render(){
+    console.log(this.state)
     return (
       <div className="App">
-        <Home closeHandler={this.closeHandler}/>
-        <Navbar open={this.state.open} clickHandler={this.clickHandler} closeHandler={this.closeHandler}/>
-        <About open={this.state.open} />
-        <Projects open={this.state.open}/>
-        <Contact open={this.state.open}/>
-        <Footer/>
+        {this.state.loading?
+        <div className="wholeapp">
+          <Home closeHandler={this.closeHandler}/>
+          <Navbar open={this.state.open} clickHandler={this.clickHandler} closeHandler={this.closeHandler}/>
+          <About open={this.state.open} />
+          <Projects open={this.state.open}/>
+          <Contact open={this.state.open}/>
+          <Footer/>
+        </div>
+        :
+        <Loading finishloading={this.finishloading}/>}
       </div>
     )
   }
