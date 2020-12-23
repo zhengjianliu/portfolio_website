9e7dd1ececebf497b291411c51975cc6eb870c04
zhengjianliu <comezheng@gmail.com>
2020-12-23 11:00:45 -0500

    update typewaiter

---
+++ src/components/Typewriter.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react'
+
+
+
+export default class Typewriter extends Component{
+  state={
+    titles:[],
+    target:0,
+    letters:-1,
+    count:-1,
+    typing:"",
+  }
+
+
+  typing = () =>{
+    this.typingscript = setInterval(()=>{
+      this.setState({count:this.state.count+1,letters:this.state.letters+1})
+      this.setState({typing:this.state.titles[this.state.target].substr(0,this.state.count)})
+      if(this.state.titles[this.state.target].length===this.state.count){
+        this.checkTyping()
+      }
+    },100)
+  }
+
+  deleting = () =>{
+    this.deletingscript = setInterval(()=>{
+      this.setState({letters:this.state.letters-1})
+      this.setState({typing:this.state.titles[this.state.target].substr(0,this.state.letters)})
+      if(this.state.letters===-1){
+        this.afterDeleting()
+      }
+    },50)
+  }
+
+  checkTyping = ()=>{
+    clearInterval(this.typingscript)
+    setTimeout(()=>{
+      this.deleting()
+    },3000)
+  }
+
+  afterDeleting = () =>{
+    clearInterval(this.deletingscript)
+    this.setState({count:0})
+    if(this.state.titles.length===this.state.target+1){
+      this.setState({target:0})
+    }else{
+      this.setState({target:this.state.target+1})
+    }
+    this.typing()
+  }
+
+  componentDidMount(){
+    this.setState({titles:this.props.titles})
+    this.typing()
+  }
+  render(){
+    return(
+      <div>{this.state.typing}<b className="typerwriter">|</b></div>
+    )
+  }
+}
