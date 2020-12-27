358531564db940014ec9da77c2639f5050c155f6
zhengjianliu <comezheng@gmail.com>
2020-12-26 20:51:18 -0500

    update

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -8,12 +8,12 @@ class Navbar extends Component{
       <div className="navbarcontainer" >
         <div id={this.props.nightmode?null:"light"} className={this.props.open?"navbar":"navbar active"}>
           <ul className="items" onClick={this.props.closeHandler}>
-            <li><Link activeClass="active" spy={true} offset={0} to="home" smooth={true} duration={300}><b onClick={this.props.closeHandler}>Home</b></Link></li>
-            <li><Link activeClass="active" spy={true} offset={0} to="about" smooth={true} duration={300}><b onClick={this.props.closeHandler}>About</b></Link></li>
-            <li><Link activeClass="active" spy={true} offset={0} to="skills" smooth={true} duration={300}><b onClick={this.props.closeHandler}>Skills</b></Link></li>
-            <li><Link activeClass="active" spy={true} offset={0} to="projects" smooth={true} duration={300}><b onClick={this.props.closeHandler}>Projects</b></Link></li>
-            <li><Link activeClass="active" spy={true} offset={0} to="blogs" smooth={true} duration={300}><b onClick={this.props.closeHandler}>Blogs</b></Link></li>
-            <li><Link activeClass="active" spy={true} offset={0} to="contact" smooth={true} duration={300}><b onClick={this.props.closeHandler}>Contact</b></Link></li>
+            <li><Link activeClass="active" spy={true} offset={0} to="home" smooth={true} duration={0}><b onClick={this.props.closeHandler}>Home</b></Link></li>
+            <li><Link activeClass="active" spy={true} offset={0} to="about" smooth={true} duration={0}><b onClick={this.props.closeHandler}>About</b></Link></li>
+            <li><Link activeClass="active" spy={true} offset={0} to="skills" smooth={true} duration={0}><b onClick={this.props.closeHandler}>Skills</b></Link></li>
+            <li><Link activeClass="active" spy={true} offset={0} to="projects" smooth={true} duration={0}><b onClick={this.props.closeHandler}>Projects</b></Link></li>
+            <li><Link activeClass="active" spy={true} offset={0} to="blogs" smooth={true} duration={0}><b onClick={this.props.closeHandler}>Blogs</b></Link></li>
+            <li><Link activeClass="active" spy={true} offset={0} to="contact" smooth={true} duration={0}><b onClick={this.props.closeHandler}>Contact</b></Link></li>
           </ul>
           <div className={this.props.open?"hamburger":"hamburger active"} onClick={this.props.clickHandler}>
             <li className="line"></li>
