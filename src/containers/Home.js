1f386f1a8f7d762d2e4609dd6672a6d1f36add0e
zhengjianliu <comezheng@gmail.com>
2021-01-06 19:16:17 -0500

    new animation added

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -7,15 +7,26 @@ import Arrow from '../images/arrow1.png'
 import Typewriter from '../components/Typewriter'
 import {connect} from 'react-redux'
 import {Link} from 'react-scroll'
+import Fade from 'react-reveal/Fade'
 
 class Home extends Component{
   render(){
     return(
       <div id="home" className="fullpage">
         <div className={this.props.nightmode? "intro": "intro light"}>
-          <h1>👋 Hey there!<br/>My name is <br/><span>Zhengjian Liu</span></h1>
-          <h2><span>I'm <Typewriter titles={['a Full Stack Developer', 'a Problem Solver 🤔','Ready To Work With You']}/></span></h2>
-          <Link to="projects" smooth={true} duration={1000} onClick={this.props.closeHandler}><img className="arrow" src={Arrow} alt="scroll down"/></Link>
+          <Fade delay={200} bottom cascade>
+            <div><h1>👋 Hey there!</h1></div>
+          </Fade>
+          <Fade delay={400} bottom cascade>
+            <div><h1>My name is</h1></div>
+          </Fade>
+          <Fade delay={600} bottom cascade>
+            <div><h1 style={{color: "#009ed9"}}>Zhengjian Liu</h1></div>
+          </Fade>
+            <h2>I'm <Typewriter titles={['a Full Stack Developer', 'a Problem Solver 🤔','Ready To Work With You']}/></h2>
+          <Fade delay={1300} bottom>
+            <Link to="projects" smooth={true} duration={1000} onClick={this.props.closeHandler}><img className="arrow" src={Arrow} alt="scroll down"/></Link>
+          </Fade>
         </div>
         <div className="infobox">
           <div>
