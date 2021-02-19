f52e4d9e9661d9eb546559fd13fad20f66c6fd5c
zhengjianliu <comezheng@gmail.com>
2021-02-19 18:29:16 -0500

    update

--- src/components/Pill.js
+++ src/components/Pill.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class Pill extends Component {
   render() {
     return (
-      <div className={`${this.props.tag} pill`}>
+      <div className='pill'>
         #{this.props.tag}
       </div>
   )
